fde2a0f637073397401c335b402cec2f11e4aa2f
Ben Pahima <118938346+Bmpahima@users.noreply.github.com>
2024-12-26 20:17:01 +0200

    Updating test.py

--- model.py
+++ model.py
@@ -2,16 +2,12 @@ import cv2
 import numpy as np
 from keras.api import models
 
-# טוען את המודל
-
-
 model = models.load_model('model.h5')
 
-##### נשים לב הפונקציה מקבלת תמונה
 def make_prediction(image):
     image = cv2.resize(image, (48, 48))
-    img = image/255 # נורמליזציה
-    img = np.expand_dims(img, axis = 0) # הרחבת המימד כי ככה המודלים מצפים לקבל את התמונות שלהם כקלט
+    img = image/255 
+    img = np.expand_dims(img, axis = 0) 
 
     class_predicted = model.predict(img)
     intId = np.argmax(class_predicted[0])
--- test.py
+++ test.py
@@ -4,54 +4,48 @@ import os
 import numpy as np
 import math
 
-# from model import make_prediction
+from model import make_prediction
 
-# שומר את התמונות 
 save_dir = 'parking_images'
 
-# אם התיקיה לא קיימת - תיצור אותה
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-# לפתוח קובץ פיקל ששומר את הנקודות
 try:
-    with open('carposition.pkl', 'rb') as f:
+    with open('parking_coordinates.pkl', 'rb') as f:
         positionList = pickle.load(f)
 except:
     positionList = []
 
 current_pos = [] 
 
+
 def get_next_id():
     return max([pos['id'] for pos in positionList]) + 1 if positionList else 0
 
-# פונקציה לחישוב המיקום של הטקסט עבור כל חנייה
+
 def mid_point(point1, point2):
     mid_x = ((point1[0] + point2[0]) / 2) - 24
     mid_y = ((point1[1] + point2[1]) / 2)
-    return (int(mid_x), int(mid_y))  # המרת ערכים לשלמים
+    return (int(mid_x), int(mid_y)) 
 
-# פונקציה לשמירת התמונות
-def save_cropped_img(img, points, id):
-    # שמירת הנקודות המערך של נאמפיי כדי שיתאימו בפורמט לפונקציה של שינוי הפרספקטיבה
-    src_points = np.array(points, dtype=np.float32)
 
-    # Calculate the width and height of the image
-    img_width = math.floor(math.dist(points[0], points[1]))
-    img_height = math.floor(math.dist(points[1], points[2]))
-    print(img_width, img_height)
+def cropped_img(img, points):
+    mask = np.zeros(img.shape[:2], dtype=np.uint8)
 
-    dst_points = np.array([[0, 0], [img_width, 0], [img_width, img_height], [0, img_height]], dtype=np.float32)
+    points = np.array(points, dtype=np.int32)
+    cv2.fillPoly(mask, [points], 255)
 
-    # מקבל את המטריצה של הפרספקטיבה
-    matrix = cv2.getPerspectiveTransform(src_points, dst_points) # התאמת פרספקטיבה
+    masked_img = cv2.bitwise_and(img, img, mask=mask)
 
-    # משנה את הפרספקטיבה של התמונה
-    cropped_img = cv2.warpPerspective(img, matrix, (img_width, img_height)) # צריך את התמונה אחרי שינוי הפרספקטיבה לצורך זיהוי אם יש שם מכונית או לא
+    x, y, w, h = cv2.boundingRect(points)
+    cropped_img = masked_img[y:y+h, x:x+w]
+    return cropped_img
 
-    # שמירת התמונה
+
+def save_img(img, points, id):
     save_path = os.path.join(save_dir, f'parking_no{id}.png')
-    cv2.imwrite(save_path, cropped_img)
+    cv2.imwrite(save_path, cropped_img(img, points))
     print(f'Saved cropped image: {save_path}')
 
 
@@ -62,20 +56,19 @@ def mouseclick(events, x, y, flags, params):
         current_pos.append((x, y))
         print(f'Point added: {x, y}')
 
-        # אם נבחרו 4 נקודות ישמור את הנקודות ויציג את החנייה
         if len(current_pos) == 4:
             positionList.append({
                 "id": get_next_id(),
                 "points": current_pos,
                 "occupied": False
             })
-            save_cropped_img(cv2.resize(cv2.imread('parking.png'), (1280, 720)), current_pos, (get_next_id() - 1)) # שמירת התמונה של החנייה
-            current_pos = []  # בחירת נקודו מחודשת
+            save_img(img=cv2.imread('parking.png'), 
+                             points=current_pos,
+                             id=(get_next_id()-1))
+            current_pos = []
 
-    # אם נלחץ על הכפתור הימני זה ימחק את החנייה
     if events == cv2.EVENT_RBUTTONDOWN:
         for i, pos in enumerate(positionList):
-            # Assuming you want to check if (x, y) is inside any saved polygon (simplification needed)
             if cv2.pointPolygonTest(np.array(pos['points'], dtype=np.int32), (x, y), False) >= 0:
                 print(f'Removed region: {pos}')
                 positionList.pop(i)
@@ -83,16 +76,13 @@ def mouseclick(events, x, y, flags, params):
                     os.remove(save_dir + f'/parking_no{pos["id"]}.png')
                 break
 
-    # שומר את הנקודות בקובץ פיקל
-    with open('carposition.pkl', 'wb') as f:
+    with open('parking_coordinates.pkl', 'wb') as f:
         pickle.dump(positionList, f)
 
 def initial_parking_mark():
     while True:
         image = cv2.imread('parking.png')
 
-        # כל חנייה מיוצגת על ידי 4 נקודות 
-        # מצייר את הצורות של החניות לפי הנקודות
         for pos in positionList:
             pts = np.array(pos['points'], np.int32).reshape((-1, 1, 2))
             cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 0), thickness=2)
@@ -104,27 +94,25 @@ def initial_parking_mark():
                         color=(0, 0, 255), 
                         thickness=2)
 
-        # כל נקודה שנבחרה לחנייה תסומן בעיגול
         for pt in current_pos:
             cv2.circle(image, pt, radius=5, color=(0, 255, 0), thickness=-1)
 
-        # מציג את התמונה ומפעיל את הevent listener
         cv2.imshow("Image", image)
         cv2.setMouseCallback("Image", mouseclick)
 
-        # אם נלחץ על התו q זה יסיים את התוכנית
         k = cv2.waitKey(1)
         if k == ord('q'):
             break
 
-        # סוגר את כל החלונות
     cv2.destroyAllWindows()
 
+def initial_prediction():
+    for pos in positionList:
+        prediction = make_prediction(image=cropped_img(img=cv2.imread('parking.png'), points=pos['points']))
+        pos['occupied'] = prediction == 'Car' 
 
 initial_parking_mark()
-# def initial_prediction():
-#     for pos in positionList:
-#         # פונקציה שמחזירה אם יש מכונית או לא
-#         prediction = make_prediction(f'parking_images/parking_no{pos["id"]}.png')
-#         pos['occupied'] = prediction == 'Car' 
 
+for pos in positionList:
+    print(f'Parking ID: {pos["id"]} is {"occupied" if pos["occupied"] else "free"}')
+    print('---------------------------')
