83305e2db7545bbbf73682f9bb2c455a6a580374
MeirHaimov <77590993+MeirHaimov@users.noreply.github.com>
2025-01-11 21:09:16 +0200

    change

--- parkingProject/parkingApp/initial.py
+++ parkingProject/parkingApp/initial.py
@@ -5,12 +5,15 @@ import numpy as np
 from YoloModels.YoloModelManager import ModelManager
 from util.image_processing import crop_image_by_points, set_text_position, get_first_frame
 from util.license_api import get_car_detail
+from .models import Parking,ParkingLot
 
 
 save_dir = 'images/parking_images'
 
 # ######### אם נרצה שזו תהיה תמונה ספציפית: ########
 # נשנה את הנתיב הבא לנתיב של התמונה שלנו
+
+
 original_img_path = 'images/fourcars.jpg'
 
 if not os.path.exists(save_dir):
@@ -29,11 +32,11 @@ def get_next_id():
     return max([pos['id'] for pos in positionList]) + 1 if positionList else 0
 
 
-def save_img(img, points, id):
-    save_path = os.path.join(save_dir, f'parking_no{id}.png')
+# def save_img(img, points, id):
+#     save_path = os.path.join(save_dir, f'parking_no{id}.png')
 
-    cv2.imwrite(save_path, crop_image_by_points(img, points))
-    print(f'Saved cropped image: {save_path}')
+    # cv2.imwrite(save_path, crop_image_by_points(img, points))
+    # print(f'Saved cropped image: {save_path}')
 
 
 def mouseclick(events, x, y, flags, params):
@@ -96,6 +99,29 @@ def initial_parking_mark():
 
     cv2.destroyAllWindows()
 
+def save_to_db(name,payment,long,lat):
+    parking_lot = ParkingLot(
+        parking_spots=len(positionList),
+        name=name,
+        payment=payment,
+        frame_image=original_img_path,
+        long=long,
+        lat=lat
+    )
+    parking_lot.save()
+
+
+
+
+
+
+    # parking_spots = models.IntegerField()
+    # name = models.CharField(max_length=50)
+    # payment = models.BooleanField(default=False)
+    # frame_image = models.CharField(max_length=250)
+    # long = models.DecimalField(max_digits=9, decimal_places=6)
+    # lat = models.DecimalField(max_digits=9, decimal_places=6)
+
 
 if __name__ == "__main__":
 
@@ -108,27 +134,31 @@ if __name__ == "__main__":
 
     initial_parking_mark()
 
-    model = ModelManager()
+    
 
-    parking_image = cv2.imread(original_img_path)
+    # model = ModelManager()
 
-    for i in range(len(positionList)):
-        image = crop_image_by_points(parking_image, positionList[i]["points"]) # the image of the parking
-        vehicle_predictions = model.free_or_occupied_prediction(image)
-        print(f" ========== car number {i} car prediction: ==========\n")
-        print(vehicle_predictions)
+    # parking_image = cv2.imread(original_img_path)
 
-        if vehicle_predictions and vehicle_predictions[0][0] in [0, 1]:
-            plate_prediction = model.license_plate_prediction(image)
-            print(f" ========== car number {i} license prediction: ==========\n")
-            print(plate_prediction)
+    # for i in range(len(positionList)):
+    #     image = crop_image_by_points(parking_image, positionList[i]["points"]) # the image of the parking
+    #     vehicle_predictions = model.free_or_occupied_prediction(image)
+    #     print(f" ========== car number {i} car prediction: ==========\n")
+    #     print(vehicle_predictions)
 
-            if plate_prediction:
-                license_plate_img = crop_image_by_points(image, plate_prediction[1])
-                number_prediction = model.license_number_prediction(license_plate_img)
-                print(f"========== car number {i} number prediction: ==========\n")
-                print(number_prediction)
+    #     if vehicle_predictions and vehicle_predictions[0][0] in [0, 1]:
+    #         plate_prediction = model.license_plate_prediction(image)
+    #         print(f" ========== car number {i} license prediction: ==========\n")
+    #         print(plate_prediction)
 
-                if number_prediction:
-                    vehicle_type = get_car_detail(number_prediction[1])
-                    print(vehicle_type)
+    #         if plate_prediction:
+    #             license_plate_img = crop_image_by_points(image, plate_prediction[1])
+    #             number_prediction = model.license_number_prediction(license_plate_img)
+    #             print(f"========== car number {i} number prediction: ==========\n")
+    #             print(number_prediction)
+
+    #             if number_prediction:
+    #                 vehicle_type = get_car_detail(number_prediction[1])
+    #                 print(vehicle_type)
+
+    
--- parkingProject/parkingApp/models.py
+++ parkingProject/parkingApp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+
+
 class ParkingLot(models.Model):
 
     parking_spots = models.IntegerField()
@@ -9,10 +11,32 @@ class ParkingLot(models.Model):
     long = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
 
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
 
 class Parking(models.Model):
 
     occupied = models.BooleanField(default=False)
-    coords = ArrayField(models.DecimalField(),size=4)
+    coords = ArrayField(models.DecimalField(decimal_places=8,max_digits=9),size=4)
     parking_lot = models.ForeignKey(ParkingLot,on_delete=models.CASCADE,related_name="parkings")
 
+    def __str__(self):
+        return f"{self.id}"
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, max_length=250)
+    phone_number = models.CharField(max_length=10)
+    password = models.TextField()
+    license_number = models.CharField(max_length=8)
+    car_type = models.CharField(max_length=50, null=True)
+    car_year = models.PositiveIntegerField(null=True)
+    car_color = models.CharField(max_length=50, null=True)
+    car_model = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
