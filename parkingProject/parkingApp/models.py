1a8f27f1164207e535f6cc0e029f82e2e7351dad
MeirHaimov <77590993+MeirHaimov@users.noreply.github.com>
2025-01-11 20:35:53 +0200

    hello
    123

--- parkingProject/parkingApp/models.py
+++ parkingProject/parkingApp/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+class ParkingLot(models.Model):
 
-# Create your models here.
+    parking_spots = models.IntegerField()
+    name = models.CharField(max_length=50)
+    payment = models.BooleanField(default=False)
+    frame_image = models.CharField(max_length=250)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
 
+
+class Parking(models.Model):
+
+    occupied = models.BooleanField(default=False)
+    coords = ArrayField(models.DecimalField(),size=4)
+    parking_lot = models.ForeignKey(ParkingLot,on_delete=models.CASCADE,related_name="parkings")
 
