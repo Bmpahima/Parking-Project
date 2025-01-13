c7d30599e8a3be259d054e1fea1be7c9320b08a4
MeirHaimov <77590993+MeirHaimov@users.noreply.github.com>
2025-01-13 18:05:33 +0200

    raz 0

---
+++ parkingProject/parkingApp/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.core.validators import RegexValidator
+from .models import User
+from django.core.validators import MinLengthValidator,MaxLengthValidator
+
+
+
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$',
+                message="Password must contain at least 8 characters, including 1 uppercase letter, 1 lowercase letter, and 1 number."
+            )
+        ]
+    )
+    phone_number = forms.CharField(
+        max_length=10,
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r'^05\d{8}$',
+                message="Phone number must start with '05' and contain 10 digits."
+            )
+        ]
+    )
+    license_plate_number = forms.CharField(
+        max_length=8,
+        required=True,
+        validators=[
+            RegexValidator(
+            regex=r'^\d{7,8}$',
+            message="license plate number contain 7-8 digits."
+            )]
+        )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'license_plate']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
