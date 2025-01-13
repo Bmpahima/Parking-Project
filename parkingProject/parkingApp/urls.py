734280b586c163e7a0c84f9b745a272b9912254e
Raz Kalfon <133892470+razkalfon@users.noreply.github.com>
2025-01-13 16:42:32 +0200

    4

--- parkingProject/parkingApp/urls.py
+++ parkingProject/parkingApp/urls.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-#קובץ נתיבים 
-=======
 from django.urls import path
 from .views import ParkingLotProvider
 
 urlpatterns = [
     path("<id>", ParkingLotProvider.as_view())
 ]
->>>>>>> origin/BEN-BRANCH
