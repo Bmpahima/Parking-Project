37e6fd7b7ca9f7ce893fa79c67c11b9d94f7992f
MeirHaimov <77590993+MeirHaimov@users.noreply.github.com>
2025-01-13 10:47:32 +0200

    Update initial.py

--- parkingProject/parkingApp/initial.py
+++ parkingProject/parkingApp/initial.py
@@ -1,20 +1,17 @@
+import os
+import django
+
+django.setup()
+
 import cv2
 import pickle
 import os
 import numpy as np
-from YoloModels.YoloModelManager import ModelManager
-from util.image_processing import crop_image_by_points, set_text_position, get_first_frame
-from util.license_api import get_car_detail
+from parkingApp.util.image_processing import set_text_position, get_first_frame
+from parkingApp.models import Parking, ParkingLot
 
 
-save_dir = 'images/parking_images'
-
-# ######### אם נרצה שזו תהיה תמונה ספציפית: ########
-# נשנה את הנתיב הבא לנתיב של התמונה שלנו
-original_img_path = 'images/fourcars.jpg'
-
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+original_img_path = './parkingApp/images/fourcars.jpg'
 
 try:
     with open('parking_coordinates.pkl', 'rb') as f:
@@ -29,11 +26,11 @@ def get_next_id():
     return max([pos['id'] for pos in positionList]) + 1 if positionList else 0
 
 
-def save_img(img, points, id):
-    save_path = os.path.join(save_dir, f'parking_no{id}.png')
+# def save_img(img, points, id):
+#     save_path = os.path.join(save_dir, f'parking_no{id}.png')
 
-    cv2.imwrite(save_path, crop_image_by_points(img, points))
-    print(f'Saved cropped image: {save_path}')
+    # cv2.imwrite(save_path, crop_image_by_points(img, points))
+    # print(f'Saved cropped image: {save_path}')
 
 
 def mouseclick(events, x, y, flags, params):
@@ -47,13 +44,7 @@ def mouseclick(events, x, y, flags, params):
             positionList.append({
                 "id": get_next_id(),
                 "points": current_pos,
-                "occupied": False,
-                "license_number": "not found"
             })
-            save_img(img=cv2.imread(original_img_path), 
-                     points=current_pos,
-                     id=(get_next_id() - 1))
-            
             current_pos = []
 
     if events == cv2.EVENT_RBUTTONDOWN:
@@ -61,8 +52,6 @@ def mouseclick(events, x, y, flags, params):
             if cv2.pointPolygonTest(np.array(pos['points'], dtype=np.int32), (x, y), False) >= 0:
                 print(f'Removed: {pos}')
                 positionList.pop(i)
-                if os.path.exists(save_dir + f'/parking_no{pos["id"]}.png'):
-                    os.remove(save_dir + f'/parking_no{pos["id"]}.png')
                 break
 
     with open('parking_coordinates.pkl', 'wb') as f:
@@ -96,39 +85,95 @@ def initial_parking_mark():
 
     cv2.destroyAllWindows()
 
+def save_to_db(name, payment, long, lat): 
+    parking_lot = ParkingLot(
+        parking_spots=len(positionList),
+        name=name,
+        payment=payment,
+        frame_image=original_img_path,
+        long=long,
+        lat=lat
+    )
+
+    parking_lot.save()
+
+    parkings = []
+    for p in positionList:
+        new_parking = Parking(occupied=False, coords=p['points'], parking_lot=parking_lot)
+        parkings.append(new_parking)
+
+    Parking.objects.bulk_create(parkings) # sending to db in one time
+
 
 if __name__ == "__main__":
 
-    if not os.path.exists(original_img_path):
-        frame = get_first_frame("images/sce_parking.mp4")
-        if frame is None:
-            exit(1)
+    # if not os.path.exists(original_img_path):
+    #     frame = get_first_frame("images/sce_parking.mp4")
+    #     if frame is None:
+    #         exit(1)
 
-        cv2.imwrite(original_img_path, frame)
+    #     cv2.imwrite(original_img_path, frame)
 
     initial_parking_mark()
 
-    model = ModelManager()
+    entered_q = input("Did you finish to mark your parking spots? (y/n)").strip()
 
-    parking_image = cv2.imread(original_img_path)
+    if entered_q =='y' or entered_q =='Y':
+        try:
+            name = input("What's the parking lot name? ").strip()
+            pay = input("Does the parking lot have some payments? (y/n) ").strip()
 
-    for i in range(len(positionList)):
-        image = crop_image_by_points(parking_image, positionList[i]["points"]) # the image of the parking
-        vehicle_predictions = model.free_or_occupied_prediction(image)
-        print(f" ========== car number {i} car prediction: ==========\n")
-        print(vehicle_predictions)
+            if pay =='y' or pay =='Y':
+                pay = True
+            elif pay == 'n' or pay == 'N':
+                pay = False
 
-        if vehicle_predictions and vehicle_predictions[0][0] in [0, 1]:
-            plate_prediction = model.license_plate_prediction(image)
-            print(f" ========== car number {i} license prediction: ==========\n")
-            print(plate_prediction)
+            long = float(input("What's the long of the parking lot? ").strip())
+            lat = float(input("What's the lat of the parking lot? ").strip())
 
-            if plate_prediction:
-                license_plate_img = crop_image_by_points(image, plate_prediction[1])
-                number_prediction = model.license_number_prediction(license_plate_img)
-                print(f"========== car number {i} number prediction: ==========\n")
-                print(number_prediction)
+            save_to_db(name, pay, long, lat)
 
-                if number_prediction:
-                    vehicle_type = get_car_detail(number_prediction[1])
-                    print(vehicle_type)
+            if os.path.exists('parking_coordinates.pkl'):
+                os.remove('parking_coordinates.pkl')
+
+        except Exception as e:
+            print("Error occoured: ", e)
+
+    elif entered_q =='n' or entered_q =='N':
+        pass
+        
+        
+
+
+
+
+
+
+    
+
+    # model = ModelManager()
+
+    # parking_image = cv2.imread(original_img_path)
+
+    # for i in range(len(positionList)):
+    #     image = crop_image_by_points(parking_image, positionList[i]["points"]) # the image of the parking
+    #     vehicle_predictions = model.free_or_occupied_prediction(image)
+    #     print(f" ========== car number {i} car prediction: ==========\n")
+    #     print(vehicle_predictions)
+
+    #     if vehicle_predictions and vehicle_predictions[0][0] in [0, 1]:
+    #         plate_prediction = model.license_plate_prediction(image)
+    #         print(f" ========== car number {i} license prediction: ==========\n")
+    #         print(plate_prediction)
+
+    #         if plate_prediction:
+    #             license_plate_img = crop_image_by_points(image, plate_prediction[1])
+    #             number_prediction = model.license_number_prediction(license_plate_img)
+    #             print(f"========== car number {i} number prediction: ==========\n")
+    #             print(number_prediction)
+
+    #             if number_prediction:
+    #                 vehicle_type = get_car_detail(number_prediction[1])
+    #                 print(vehicle_type)
+
+    
