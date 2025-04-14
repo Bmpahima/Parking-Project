071dfa1a3be7c5d5cf9ad0c6be44f06f0a070a40
Ben Pahima <118938346+Bmpahima@users.noreply.github.com>
2025-04-14 18:35:24 +0300

    test

--- parkingProject/parkingApp/routing.py
+++ parkingProject/parkingApp/routing.py
@@ -1,6 +1,10 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from .consumers import VideoConsumer
 
+# websocket_urlpatterns = [
+#     re_path(r'ws/video/$', VideoConsumer.as_asgi()),
+# ]
+
 websocket_urlpatterns = [
-    re_path(r'ws/video/$', VideoConsumer.as_asgi()),
+    path('ws/video/', VideoConsumer.as_asgi()),
 ]
