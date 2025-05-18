b31ab89881620ba6617cc65deb75578eb8bf5818
MeirHaimov <meir965@gmail.com>
2025-05-18 17:17:38 +0300

    commit

--- parkingProject/parkingApp/urls.py
+++ parkingProject/parkingApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ParkingLotProvider, AllParkingLot, SaveParking, ReleaseParking, getOwnerParkingLot, getParkingLotUsers, GetParkingStats
 # from .views import UserRegistrationView, UserLoginView
 
-
+# URL Configuration for parkingApp endpoints
 urlpatterns = [
     path("<int:id>", ParkingLotProvider.as_view()),
     path("all/",AllParkingLot.as_view()),
